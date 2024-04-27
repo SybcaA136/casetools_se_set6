212f1c9fe61b40a062e51243d01457c4d830e455
franela <franela@gmail.com>
2024-04-27 14:14:48 +0530

    commit

---
+++ casetools/src/casetools/rectangle.java
@@ -0,0 +1,47 @@
+package casetools;
+
+public class rectangle {
+	private double width;
+	private double height;
+	public  rectangle(double width,double height) {
+		this.width=width;
+		this.height=height;
+	}
+	public double getwidth() {
+		return width;
+		
+	}
+	public void setwidth(double width) {
+		this.width=width;
+	}
+	public double getheight() {
+		return height;
+	}
+	public void setheight(double height) {
+		this.height=height;
+	}
+	public static void main(String[]args) {
+		//TODO Auto-generated method stub
+	}
+	public double getArea() {
+		return width*height;
+	}
+	public double getperimeter() {
+		return 2* (width+height);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
